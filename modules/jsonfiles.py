8ceccbc6dfb0420c86b0e93312067b086336e3c7
Jhoan Landazabal <100872821+JhoanSLC@users.noreply.github.com>
2024-03-08 03:19:31 +0000

    Library start

---
+++ modules/jsonfiles.py
@@ -0,0 +1,24 @@
+import modules.screen as scr
+import json
+import os 
+
+BASE = 'data/'
+#Checks if json file exists and if it doesn't then creates it
+#receive: json file name
+def check_json(file : str):
+  if not os.path.isfile(BASE+file): #Checks if file exists
+    with open(BASE+file, 'w') as create:
+      json.dump({}, create, indendt = 4) #Creates file
+
+#Read json file and converts it into a python variable
+#recive: Json file name
+#return: Json file as a python variable
+def read_json(file : str) -> dict:
+  with open(BASE+file, 'r') as read:
+    return json.load(read)
+
+#update json file with new given data
+#recive: Json file name, new data
+def update_json(file : str, new_data : dict):
+  with open(BASE+file, 'w') as update:
+    json.dump(new_data, update, indent = 4)
---
+++ modules/screen.py
@@ -0,0 +1,19 @@
+import os 
+import sys
+
+#Clean screen based on the operative system
+def clean():
+  if sys.platform == 'windows': #if operative system is windows
+    os.system('cls')
+  elif sys.platform == 'linux': #If operative system is linux
+    os.system('clear')
+  else:
+    os.system('cls')
+
+#Pause screen based on the operative system
+def pause():
+  if sys.platform == 'windows': #if operative system is windows
+    os.system('pause')
+  elif sys.platform == 'linux': #If operative system is linux
+    os.system('read -p "Press any key to continue..."')
+
